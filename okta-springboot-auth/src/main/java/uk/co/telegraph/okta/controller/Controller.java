6100ddf05a168ac5f6c9cc4327cf173dd3fb2152
thiwwy <thiwwy@hotmail.it>
2020-01-23 17:55:42 +0000

    .

--- okta-springboot-auth/src/main/java/uk/co/telegraph/okta/controller/Controller.java
+++ okta-springboot-auth/src/main/java/uk/co/telegraph/okta/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RedirectController {
+public class Controller {
 
   @GetMapping("/api/hello")
   public ResponseEntity<String> getHello() {
